293585467dbefb02b352a33b1c15965a1ce6d3d3
JoseHer <josejh3010@gmail.com>
2026-05-28 17:48:34 -0500

    feat: update maintenance queries to sql/

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,18 +1,24 @@
 
--- Locations sin mapear
+-- Locations with unknown country
 SELECT location_name, country
 FROM dim_location
 WHERE country = 'Unknown'
 ORDER BY location_name;
 
--- Actualizar country
+-- Update country
 UPDATE dim_location
 SET country = ''
 WHERE location_name = '';
 
 
--- Tags sin clasificar
+-- Tags without category
 SELECT tag_name, tag_category
 FROM dim_tag
 WHERE tag_category = 'other'
 ORDER BY tag_name;
+
+-- Update tag_category
+UPDATE dim_tag
+SET tag_category = 'technology'
+WHERE tag_name IN ('', '', '');
+
